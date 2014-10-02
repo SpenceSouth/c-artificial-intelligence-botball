219616de1a7f7cf1926bd837d2dac34a5033ac01
Aaron Wagner <aaron.p.wagner@gmail.com>
2014-10-02 15:10:16 -0400

    Fixed the double main caused by putting linetest in the same directory as main.c

--- main/Linetest2/Linetest2.c
+++ main/Linetest2/Linetest2.c
@@ -3,8 +3,8 @@ int main()
 	int rport=0;
 	int cport=1; 
 	int lport=2;
-	leftmtr=0; 
-	rghtmtr=1; 			// identify port and motors
+	int leftmtr=0; 
+	int rghtmtr=1; 			// identify port and motors
 	int threshold=400; 									// set threshold for light conditions
 	int high=60,low=-20; 								// set wheel powers for arc radius
 	/*if (analog10(cport)<500)
@@ -19,7 +19,7 @@ int main()
 				motor(leftmtr,low); 
 				motor(rghtmtr,-1*(high+11)); 			// arc left
 				
-				printf( "Arcing left: Analog10 = %d  T: %d \n",  analog10(cport), threshold);
+				printf( "Left: C: %d L: %d R: %D  T: %d \n",  analog10(cport), analog10(lport), analog10(rport),  threshold);
 				
 				if (side_button()!=0) 
 					break; 
@@ -28,7 +28,7 @@ int main()
 			{ 				// continue until dark
 				motor(leftmtr,high); 
 				motor(rghtmtr,-1*(low+11)); 			// arc right
-				printf( "Arcing right: Analog10 = %d T: %d \n",  analog10(cport), threshold);
+				printf( "Arcing right: C: %d T: %d \n",  analog10(cport), analog10(lport), analog10(rport), threshold);
 				
 				if (side_button()!=0) 
 					break; 
